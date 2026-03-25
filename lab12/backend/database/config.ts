aa36a98acf6049c0d97c840c529899a61d1f8936
RomanPR <prikhodjko.roman@gmail.com>
2026-03-25 03:22:50 +0200

    check

--- lab12/backend/database/config.ts
+++ lab12/backend/database/config.ts
@@ -1,25 +1,13 @@
 import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
-const DB_HOST = process.env['DB_HOST'] || 'localhost';
-const DB_PORT = parseInt(process.env['DB_PORT'] || '5432');
-const DB_NAME = process.env['DB_NAME'] || 'coworking_db';
-const DB_USER = process.env['DB_USER'] || 'postgres';
-const DB_PASSWORD = process.env['DB_PASSWORD'] || 'postgres';
-
-export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
-    port: DB_PORT,
+export const sequelize = new Sequelize(process.env['DATABASE_URL']!, {
     dialect: 'postgres',
     logging: process.env['NODE_ENV'] === 'development' ? console.log : false,
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-    define: {
-        timestamps: false,
-        underscored: true,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
     },
 });
